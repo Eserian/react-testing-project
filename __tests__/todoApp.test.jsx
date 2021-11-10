567a946634b92200424b2ec3b5159ee5852a3031
Evgenii Shevchenko <evgenii@Evgeniis-MacBook-Air.local>
2021-11-11 00:51:30 +0300

    fix tests

--- __tests__/todoApp.test.jsx
+++ __tests__/todoApp.test.jsx
@@ -11,90 +11,100 @@ afterAll(() => server.close());
 beforeEach(() => render(<App />));
 
 const addTask = async (taskName) => {
-  userEvent.type(screen.getByRole('textbox', { name: 'New task' }), taskName);
-  userEvent.click(screen.getByRole('button', { name: 'Add' }));
-  await waitFor(() => {
-    expect(screen.getByRole('checkbox', { name: taskName })).toBeInTheDocument();
-  });
+  userEvent.type(screen.getByRole('textbox', { name: /^new task$/i }), taskName);
+  userEvent.click(screen.getByRole('button', { name: /^add$/i }));
 };
 
 const addList = async (listName) => {
-  userEvent.type(screen.getByRole('textbox', { name: 'New list' }), listName);
-  userEvent.click(screen.getByRole('button', { name: 'add list' }));
-  await waitFor(() => {
-    expect(screen.getByRole('button', { name: listName })).toBeInTheDocument();
-  });
+  userEvent.type(screen.getByRole('textbox', { name: /^new list$/i }), listName);
+  userEvent.click(screen.getByRole('button', { name: /^add list$/i }));
 };
 
-it('should render page', () => {
-  expect(screen.getByRole('textbox', { name: 'New list' })).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: 'add list' })).toBeInTheDocument();
-  expect(screen.getByRole('textbox', { name: 'New task' })).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: 'Add' })).toBeInTheDocument();
-});
+describe('tasks', () => {
+  it('should toggle tasks state', async () => {
+    addList('primary');
+    await screen.findByRole('button', { name: /primary/i });
+    addTask('first task');
+    const firstTaskCheckbox = await screen.findByRole('checkbox', { name: /first task/i });
+    addTask('second task');
+    const secondTaskCheckbox = await screen.findByRole('checkbox', { name: /second task/i });
+    expect(firstTaskCheckbox).not.toBeChecked();
+    expect(secondTaskCheckbox).not.toBeChecked();
 
-it('should interact with tasks', async () => {
-  await addList('primary');
-  await addTask('first task');
-
-  const newTaskCheckbox = screen.getByRole('checkbox', { name: 'first task' });
-  expect(newTaskCheckbox).not.toBeChecked();
-
-  userEvent.click(newTaskCheckbox);
-  await waitFor(() => {
-    expect(newTaskCheckbox).toBeChecked();
+    userEvent.click(firstTaskCheckbox);
+    await waitFor(() => {
+      expect(firstTaskCheckbox).toBeChecked();
+    });
+    expect(secondTaskCheckbox).not.toBeChecked();
   });
 
-  userEvent.click(screen.getByRole('button', { name: 'Remove' }));
-  await waitFor(() => {
-    expect(newTaskCheckbox).not.toBeInTheDocument();
+  it('should delete task', async () => {
+    addList('primary');
+    await screen.findByRole('button', { name: /primary/i });
+    addTask('first task');
+    const firstTask = await screen.findByRole('checkbox', { name: /first task/i });
+    addTask('second task');
+    const secondTask = await screen.findByRole('checkbox', { name: /second task/i });
+
+    const deleteFirstTaskBtn = screen.getAllByRole('button', { name: /remove/i })[1];
+    userEvent.click(deleteFirstTaskBtn);
+    await waitFor(() => {
+      expect(firstTask).not.toBeInTheDocument();
+    });
+    expect(secondTask).toBeInTheDocument();
   });
 });
 
-it('should not create lists with the same name', async () => {
-  await addList('new list');
-  await addList('new list');
-  const invalidFeedback = await screen.findByText(/new list already exists/i);
-  expect(invalidFeedback).toBeInTheDocument();
-});
-
-it('should delete tasks with list', async () => {
-  await addList('new list');
-  await addTask('first task');
-  await addTask('second task');
-  const firstTask = screen.getByRole('checkbox', { name: 'first task' });
-  const secondTask = screen.getByRole('checkbox', { name: 'second task' });
-
-  const removeListBtn = screen.getByRole('button', { name: 'remove list' });
-  userEvent.click(removeListBtn);
-  await waitFor(() => {
-    expect(removeListBtn).not.toBeInTheDocument();
+describe('lists', () => {
+  it('should not create lists with the same name', async () => {
+    addList('new list');
+    await screen.findByRole('button', { name: /new list/i });
+    addList('new list');
+    const invalidFeedback = await screen.findByText(/new list already exists/i);
+    expect(invalidFeedback).toBeInTheDocument();
   });
 
-  await addList('new list');
-  expect(firstTask).not.toBeInTheDocument();
-  expect(secondTask).not.toBeInTheDocument();
-});
+  it('should delete tasks with list', async () => {
+    addList('new list');
+    const list = await screen.findByRole('button', { name: /new list/i });
+    addTask('first task');
+    const firstTask = await screen.findByRole('checkbox', { name: /first task/i });
+    addTask('second task');
+    const secondTask = await screen.findByRole('checkbox', { name: /second task/i });
 
-it('should not affect other lists', async () => {
-  await addList('new list');
-  await addTask('first list task');
+    const removeListBtn = screen.getByRole('button', { name: /remove list/i });
+    userEvent.click(removeListBtn);
+    await waitFor(() => {
+      expect(list).not.toBeInTheDocument();
+    });
 
-  const newList = screen.getByRole('button', { name: 'new list' });
-  const firstTask = screen.getByRole('checkbox', { name: 'first list task' });
-
-  await addList('another list');
-  await addTask('another list task');
-
-  const anotherFirstTask = screen.getByRole('checkbox', { name: 'another list task' });
-
-  userEvent.click(anotherFirstTask);
-  await waitFor(() => {
-    expect(anotherFirstTask).toBeChecked();
+    addList('new list');
+    await screen.findByRole('button', { name: /new list/i });
+    expect(firstTask).not.toBeInTheDocument();
+    expect(secondTask).not.toBeInTheDocument();
   });
 
-  userEvent.click(newList);
-  await waitFor(() => {
-    expect(firstTask).not.toBeChecked();
+  it('should not affect other lists', async () => {
+    addList('new list');
+    const newList = await screen.findByRole('button', { name: /new list/i });
+
+    addTask('first list task');
+    const firstTask = await screen.findByRole('checkbox', { name: /first list task/i });
+
+    addList('another list');
+    await screen.findByRole('button', { name: /another list/i });
+    addTask('another list task');
+
+    const anotherFirstTask = await screen.findByRole('checkbox', { name: /another list task/i });
+
+    userEvent.click(anotherFirstTask);
+    await waitFor(() => {
+      expect(anotherFirstTask).toBeChecked();
+    });
+
+    userEvent.click(newList);
+    await waitFor(() => {
+      expect(firstTask).not.toBeChecked();
+    });
   });
 });
